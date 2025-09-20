2ca1209e93d951ab96504760e9f299f7613d9bf1
JC9999999 <robotic32100@gmail.com>
2025-09-20 17:24:01 -0400

    Added repeat string solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    let i = 0;
+    let result = "";
+    while(i < num){
+        //code for adding strings together
+        result = result + string;
+        i++;
+    }
+    return result;
 };
 
 // Do not edit below this line
